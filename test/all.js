a63e5e74d9d7c1845efd8b3f90ab161de337115b
Wai Ting Cheung <waitingcheung@users.noreply.github.com>
2017-07-12 21:35:49 +0800

    Update package.json

--- test/all.js
+++ test/all.js
@@ -13,7 +13,7 @@ test('isSameChange', function () {
         content: '-filename.js:7:1~7:17: [Warning] Conditional expression \'cond === null\' is always true.'
     };
 
-    ok(isSameChange(delChange, addChange), 'Both changes are the same');
+    assert.ok(isSameChange(delChange, addChange), 'Both changes are the same');
 });
 
 test('filterChanges', function () {
@@ -33,35 +33,35 @@ test('filterChanges', function () {
 
     var changes = [delChange, addChange];
     var filteredChanges = filterChanges(changes);
-    ok(filteredChanges.length === 0, 'Same changes filtered');
+    assert.ok(filteredChanges.length === 0, 'Same changes filtered');
 });
 
-test('read a diff with 1 warning', function () {
-    expect(1);
-    stop();
+test('read a diff with 1 warning', function (assert) {
+    assert.expect(1);
+    var done = assert.async(1);
 
     setTimeout(function () {
         var config = {
             'file': 'test/examples/warning1.diff'
         };
         readFile(config, function (warnings) {
-            ok(warnings.length === 1, '1 warning found');
-            start();
+            assert.ok(warnings.length === 1, '1 warning found');
+            done();
         });
     }, 0);
 });
 
-test('read a diff with no warnings', function () {
-    expect(1);
-    stop();
+test('read a diff with no warnings', function (assert) {
+    assert.expect(1);
+    var done = assert.async(1);
 
     setTimeout(function () {
         var config = {
             'file': 'test/examples/nowarning1.diff'
         };
         readFile(config, function (warnings) {
-            ok(warnings.length === 0, 'No warnings found');
-            start();
+            assert.ok(warnings.length === 0, 'No warnings found');
+            done();
         });
     }, 0);
 });
@@ -74,7 +74,7 @@ test('filter out conditional expression warnings', function () {
         content: '-filename.js:7:1~7:17: [Warning] Conditional expression \'cond === null\' is always true.'
     };
 
-    ok(shouldFilterWarning(change), 'Condition expression warnings filtered');
+    assert.ok(shouldFilterWarning(change), 'Condition expression warnings filtered');
 });
 
 test('filter out too many arguments warnings', function () {
@@ -85,7 +85,7 @@ test('filter out too many arguments warnings', function () {
         content: '-filename.js:7:1~7:17: [Warning] Too many arguments to function \'foo\'.'
     };
 
-    ok(shouldFilterWarning(change), 'Too many arguments warnings filtered');
+    assert.ok(shouldFilterWarning(change), 'Too many arguments warnings filtered');
 });
 
 test('filter out too few arguments warnings', function () {
@@ -96,7 +96,7 @@ test('filter out too few arguments warnings', function () {
         content: '-filename.js:7:1~7:17: [Warning] Too few arguments to function \'foo\'.'
     };
 
-    ok(shouldFilterWarning(change), 'Too few arguments warnings filtered');
+    assert.ok(shouldFilterWarning(change), 'Too few arguments warnings filtered');
 });
 
 test('filter out absent property with dynamic values warnings', function () {
@@ -107,7 +107,7 @@ test('filter out absent property with dynamic values warnings', function () {
         content: '-filename.js:7:1~7:17: [Warning] Reading absent property \'prop\' of object \'obj\', where property \'prop\' can be "val".'
     };
 
-    ok(shouldFilterWarning(change), 'Absent property with dynamic values filtered');
+    assert.ok(shouldFilterWarning(change), 'Absent property with dynamic values filtered');
 });
 
 test('filter out nth argument of prop should be of certain type', function () {
@@ -118,7 +118,7 @@ test('filter out nth argument of prop should be of certain type', function () {
         content: '-filename.js:7:1~7:17: [Warning] First argument of \'prop\' should be an object type.'
     };
 
-    ok(shouldFilterWarning(change), 'Argument of certain type filtered');
+    assert.ok(shouldFilterWarning(change), 'Argument of certain type filtered');
 });
 
 test('filter out warnings based on filename', function () {
@@ -129,7 +129,7 @@ test('filter out warnings based on filename', function () {
         content: '-filename.js:7:1~7:17: [Warning] Random message.'
     };
 
-    ok(shouldFilterWarning(change, 'filename.js'), 'Warnings from filename filtered');
+    assert.ok(shouldFilterWarning(change, 'filename.js'), 'Warnings from filename filtered');
 });
 
 test('filter out warnings based on directory', function () {
@@ -140,7 +140,7 @@ test('filter out warnings based on directory', function () {
         content: '-filename.js:7:1~7:17: [Warning] Random message.'
     };
 
-    ok(shouldFilterWarning(change, 'test/examples'), 'Warnings from directory filtered');
+    assert.ok(shouldFilterWarning(change, 'test/examples'), 'Warnings from directory filtered');
 });
 
 test('filter out warnings independent of developer mode', function () {
@@ -151,10 +151,10 @@ test('filter out warnings independent of developer mode', function () {
         content: '-filename.js:7:1~7:17: [Warning] Random message.'
     };
 
-    ok(shouldFilterWarning(change, 'filename.js', true), 'Warnings from filename filtered with developer mode');
-    ok(shouldFilterWarning(change, 'filename.js', false), 'Warnings from filename filtered without developer mode');
-    ok(shouldFilterWarning(change, 'test/examples', true), 'Warnings from directory filtered with developer mode');
-    ok(shouldFilterWarning(change, 'test/examples', false), 'Warnings from directory filtered without developer mode');
+    assert.ok(shouldFilterWarning(change, 'filename.js', true), 'Warnings from filename filtered with developer mode');
+    assert.ok(shouldFilterWarning(change, 'filename.js', false), 'Warnings from filename filtered without developer mode');
+    assert.ok(shouldFilterWarning(change, 'test/examples', true), 'Warnings from directory filtered with developer mode');
+    assert.ok(shouldFilterWarning(change, 'test/examples', false), 'Warnings from directory filtered without developer mode');
 });
 
 test('filter nothing in developer mode', function () {
@@ -165,11 +165,11 @@ test('filter nothing in developer mode', function () {
         content: '-filename.js:7:1~7:17: [Warning] Random message.'
     };
 
-    ok(!shouldFilterWarning(change, undefined, true), 'No warnings filtered');
+    assert.ok(!shouldFilterWarning(change, undefined, true), 'No warnings filtered');
 });
 
 
 test('extract the total number of warnings', function () {
     var content = '+|  Warnings          :     7 (100.00%) |';
-    ok(computeWarningsTotal(content) === 7, 'Total number of warnings extracted');
+    assert.ok(computeWarningsTotal(content) === 7, 'Total number of warnings extracted');
 });
